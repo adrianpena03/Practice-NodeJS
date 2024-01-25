7604ec13673e4440e31841285bf2d29c3e9379c6
Adrian <adrianpena03@gmail.com>
2024-01-25 14:54:04 -0500

    serv

--- basicServer.js
+++ basicServer.js
@@ -1,3 +1,26 @@
+// // Dependencies
+// const http = require('http');
+
+// // Create server object to respond to a requets with a string.
+// // Nodejs functions can be assigned to a variable. Can nest a function too
+
+// const server = http.createServer((req, res) => {
+//     res.end('Hello, World\n');
+// });
+
+// // Make server listen on port 3000 for any request
+// server.listen(3000, () => {
+//     console.log('Server is listening on port 3000.');
+// });
+
+/**
+ * 
+ * 
+ * BASIC SERVER ABOVE, ADDING MORE TO IT BELOW
+ * 
+ * 
+ */
+
 // Dependencies
 const http = require('http');
 
@@ -5,6 +28,9 @@ const http = require('http');
 // Nodejs functions can be assigned to a variable. Can nest a function too
 
 const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    console.log(req.method);
+    console
     res.end('Hello, World\n');
 });
 
@@ -12,4 +38,3 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
     console.log('Server is listening on port 3000.');
 });
-
