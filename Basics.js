8c4b55691ccc52b95cf8ef428088bcd6fbdcc431
Adrian Pena <adrianpena@arkham.lan>
2024-01-26 12:47:11 -0500

    Added arrays, hashmaps, fs

---
+++ Basics.js
@@ -0,0 +1,51 @@
+// const fs = require('fs');
+
+// //Get the file list of the current directory
+// let list = fs.readdirSync(__dirname);
+// console.log("The list of files in the directory:\n"+list);
+
+// // make a new directory called new
+// // let newdir = __dirname + '/new';
+// // fs.mkdirSync(newdir);
+
+// //Get the file list of the current directory
+// list = fs.readdirSync(__dirname,"utf-8");
+// console.log("The list of files in the directory:\n"+list);
+
+// ------------------------------------
+
+// HASHMAPS
+
+// const obj1 = {};
+
+// obj1.FirstName = 'Adrian';
+
+// // console.log(obj1) // Returns hashmap "{ FirstName: 'Adrian' }"
+
+// const obj2 = {
+//     ID: 123,
+//     Name: 'Adrian',
+//     boolean: True
+// };
+
+// obj1.print = () => { // Or function () {
+//     console.log(obj1)
+// } 
+
+// -----------------------------------
+
+// ARRAYS
+
+// const ary1 = ['Bananas', 'apples', 'oranges'];
+
+// console.log(ary1[1]); // Returns apples, index outside of list returns 'undefined'
+
+// const ary2 = [{fruit: 'apples', vegtable: 'lettuce'}, {boy: 'Sam', girl: 'yo mama'}]; // list of 2 hashmaps
+
+// const mixed = [23, 'string', {num: 45}] // Can have a list of different data types
+
+// -----------------------------------
+
+process.argv.forEach((val, index) => { // 'forEach' is a method that acts as a for loop
+    console.log(`${index}: ${val}`)
+});
--- basicServer.js
+++ basicServer.js
@@ -22,19 +22,34 @@
  */
 
 // Dependencies
+// const http = require('http');
+
+// // Create server object to respond to a requets with a string.
+// // Nodejs functions can be assigned to a variable. Can nest a function too
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.headers);
+//     console.log(req.method);
+//     console
+//     res.end('Hello, World\n');
+// });
+
+// // Make server listen on port 3000 for any request
+// server.listen(3000, () => {
+//     console.log('Server is listening on port 3000.');
+// });
+
 const http = require('http');
 
-// Create server object to respond to a requets with a string.
-// Nodejs functions can be assigned to a variable. Can nest a function too
-
 const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    console.log(req.method);
-    console
-    res.end('Hello, World\n');
+    console.log(`Req Headers ${req.headers}`);
+    console.log(`Req Method ${req.method}`);
+    console.log(`Req URL: ${req.url}`);
+    res.end('Hi, there. Welcome.');
+
 });
 
-// Make server listen on port 3000 for any request
 server.listen(3000, () => {
-    console.log('Server is listening on port 3000.');
+    console.log('Server is listening on port 3000')
 });
+
